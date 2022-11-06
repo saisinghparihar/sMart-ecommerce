73061f249796f99912d50369f8f5cdfb35a6a21d
saisinghparihar <saisinghparihar626@gmail.com>
2022-11-06 19:16:23 +0530

    add new files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.scss";
+import { useState } from "react";
+
+function App() {
+  const [first, setFirst] = useState("Sai");
+  return (
+    <div>
+      <h1>Hello World!!!</h1>
+    </div>
+  );
+}
+
+export default App;
